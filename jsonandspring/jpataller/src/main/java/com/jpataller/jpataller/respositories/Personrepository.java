2fba115a8b594da58394e147a04b709a4c922187
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-07-24 21:37:37 -0500

    add

---
+++ jsonandspring/jpataller/src/main/java/com/jpataller/jpataller/respositories/Personrepository.java
@@ -0,0 +1,13 @@
+package com.jpataller.jpataller.respositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jpataller.jpataller.entities.Person;
+
+public interface Personrepository extends CrudRepository<Person, Long>{
+
+
+
+    
+
+}
